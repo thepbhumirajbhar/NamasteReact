a1418de34e1bd2d2e49d65a815128b7ecf765998
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-01-31 03:41:33 +0530

    cdn react

---
+++ Video 1/app.js
@@ -0,0 +1,9 @@
+const heading=React.createElement("h1",
+  {
+    id:"heading",xyz:'abc'
+  },"Hello From React..!!")
+
+console.log(heading)    //returns an object 
+
+const reactRoot=ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(heading); 
