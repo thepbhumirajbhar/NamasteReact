4c8860bd23e7d208c8244d288e86b2c2dfe1f43e
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-01-31 16:46:07 +0530

    add siblings to createElement

--- Video 1/app2.js
+++ Video 1/app2.js
@@ -1,14 +1,48 @@
 /*
-React Code to get:
+REACT CODE TO GET:
 <div id="parent"
    <div id="child"
       <h1> I am the heading </h1>
    </div>
 </div>     
+
+
+
+React.createElement(object) => while rendering it converts into HTML(that browser understands)
+
+
 */
 
 
-// React.createElement(object) => while rendering it converts into HTML(that browser understands)
+
+
+// const parent=React.createElement(
+// "div",
+// {id:"parent"},
+// React.createElement(
+//   "div",
+//   {id: "child"},
+//   React.createElement("h1",{},"I am the heading")
+// )
+// );
+
+// console.log(parent);           //returns an object
+
+// const root=ReactDOM.createRoot(document.getElementById("root2"));
+
+// root.render(parent);
+
+
+
+/*
+REACT CODE TO GET:
+<div id="parent"
+   <div id="child"
+      <h1> I am the heading </h1>
+      <h2> I an the second heading </h2>
+   </div>
+</div>     
+*/
 
 const parent=React.createElement(
 "div",
@@ -16,12 +50,16 @@ const parent=React.createElement(
 React.createElement(
   "div",
   {id: "child"},
-  React.createElement("h1",{},"I am the heading")
+  React.createElement[
+    ("h1",{},"I am the heading"),
+    ("h2",{},"I am the second heading")]
 )
 );
+
+
 
 console.log(parent);           //returns an object
 
 const root=ReactDOM.createRoot(document.getElementById("root2"));
 
-root.render(parent);
+root.render(parent); 
