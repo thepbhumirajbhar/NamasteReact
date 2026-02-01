7ba70854e9829458aaf24c359afb0aab54a399fb
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-02 00:11:23 +0530

    element vs component

---
+++ Video 3/App2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
+//* FUNCTIONAL COMPONENT vs REACT ELEMENT 
+
+const heading=(
+<h1>
+  React Element
+</h1>);
+
+
+const Heading= ()=>{
+  return <h1>Functional Component</h1>
+}
+
