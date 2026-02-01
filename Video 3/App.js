193b394d64e0df2e71a8ab6514b86fc3fb738a6d
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-02 00:10:04 +0530

    React Functional Component

--- Video 3/App.js
+++ Video 3/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//1. React Elements
+
+// // 1. React Elements
 /* 
-react.createElement => React element(JS object)
-after rendering JS object => HTML element 
- */
+* react.createElement => React element(makes a JS object)
+* after rendering JS object => HTML element 
+*/
+
+
 const heading=React.createElement("h1",{id:"heading"},"I am the heading");
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
@@ -15,20 +18,38 @@ root.render(heading);
 
 //--------------------------------------------------------------------------------------------------------------------
 
-//2. INTRO TO JSX: it is Js syntax => easier to create elements, lets you write *HTML-like code* directly inside your JavaScript files.
+// // 2. INTRO TO JSX: it is Js syntax => easier to create elements, lets you write *HTML-like code* directly inside your JavaScript files.
 /* 
-JS engine does not understand code in JSX---> parcel/@babel (transpile the code b4 it reaches JS Engine)
+* JS engine does not understand code in JSX---> parcel/@babel (transpile the code b4 it reaches JS Engine)
 ************
-JSX => react.createElement (transpiled)
-react.createElement => React element(JS object)
-after rendering JS object => HTML element 
+* JSX => react.createElement (transpiled into react element)
+* react.createElement => React element(makes a JS object)
+* after rendering JS object => HTML element 
 */ 
-const jsxHeading= <h1 id="jsxHeading">Hiii in JSX Syntax</h1>;
+
+
+const jsxHeading= <h1 id="jsxHeading">Hiii in JSX Syntax</h1>;     //if multiple lines use () to wrap up
+
 const root2=ReactDOM.createRoot(document.getElementById("root2"));
 
 root2.render(jsxHeading); 
 
+//--------------------------------------------------------------------------------------------------------
 
-//--------------------------------------------------------------------------------------------------------------------
+// //3. React Component
+/*
+* 2 ways to create component: class based component(old way), functional component(now used)
+*/
 
-//3. 
+
+//* FUNCTIONAL COMPONENT: function that returns some sort of jsx
+const HeadingComponent= () => (                                        //use () agar return nhi likhna ho instead of {}
+  <div id="container">
+     <h1>Functional component starts with a Capital letter</h1>;
+  </div>                                     
+  
+);
+
+const root3=ReactDOM.createRoot(document.getElementById("root3"));
+
+root3.render(<HeadingComponent />);
