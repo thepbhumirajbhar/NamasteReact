4eefec2034d6918e32ccadf52d9ad70079a2afc2
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-05 19:17:53 +0530

    Intro to onClick attribute

--- Video 5/components/Body.js
+++ Video 5/components/Body.js
@@ -5,7 +5,13 @@ import resObj from "../utils/mockData";
 const Body= () =>{
   return(
       <div className="body">
-        <div className="search">Search</div>
+        <div className="filter">
+          <button className="filter-btn" onClick={() => {
+             console.log("button clicked")
+           }}>
+            Top Rated Restaurants
+          </button>
+        </div>
 
         <div className="res-container">
            {
