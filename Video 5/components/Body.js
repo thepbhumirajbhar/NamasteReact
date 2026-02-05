788fc68893cde1453ac2bff3d53839318e53801c
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-05 21:12:16 +0530

    Intro to hooks/ useState()

--- Video 5/components/Body.js
+++ Video 5/components/Body.js
@@ -1,10 +1,12 @@
 import RestaurantCard from "./RestaurantCard";
 //import resObj from "../utils/mockData";
+import { useState } from "react"; 
+
 
 
 
 //*normal JS variable 
-let listOfRestaurants=[
+let listOfRestaurant2=[
   {
     "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
     "info": {
@@ -51,13 +53,61 @@ let listOfRestaurants=[
 
 
 const Body= () =>{
+/*
+! STATE Variable: Super Powerful Varaible(using HOOKS)
+! (more in README.md)
+*/
+  { const [listOfRestaurants] = useState([
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "1026651",
+      "name": "Chinese Wok",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/28/a44aaf25-3312-47f1-b028-6a28e9da5513_1026651.jpg",
+      "costForTwo": "₹250 for two",
+      "cuisines": ["Chinese", "Asian", "Tibetan", "Desserts"],
+      "avgRating": 4.2,
+      "sla": {
+        "deliveryTime": 31,
+      }
+    }
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "1026652",
+      "name": "MCD",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/28/a44aaf25-3312-47f1-b028-6a28e9da5513_1026651.jpg",
+      "costForTwo": "₹250 for two",
+      "cuisines": ["Chinese", "Asian", "Tibetan", "Desserts"],
+      "avgRating": 3.6,
+      "sla": {
+        "deliveryTime": 25,
+      }
+    }
+  },
+  {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "1026653",
+      "name": "KFC",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/28/a44aaf25-3312-47f1-b028-6a28e9da5513_1026651.jpg",
+      "costForTwo": "₹250 for two",
+      "cuisines": ["Chinese", "Asian", "Tibetan", "Desserts"],
+      "avgRating": 4.8,
+      "sla": {
+        "deliveryTime": 25,
+      },
+    },
+  },
+])
   return(
-      <div className="body"> 
+      <div className="body">
         <div className="filter">
           <button className="filter-btn" onClick={() => {
              //! filter logic 
              listOfRestaurants=listOfRestaurants.filter(
-               (res) => res.info.avgRating>4);
+               (res) => res.info.avgRating>4); 
               console.log(listOfRestaurants)
            }}>
             Top Rated Restaurants
@@ -73,9 +123,9 @@ const Body= () =>{
            {/* whenever you are looping, give each element a unique key. why? */}
              
         </div>
-
      </div>
   )
+}
 }
 
 
