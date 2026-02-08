8bda9b39c3595279d520480659f0efd408e89bea
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-08 18:04:06 +0530

    same as video 5

---
+++ Video 6/components/RestaurantCard.js
@@ -0,0 +1,28 @@
+import { CDN_URL } from "../utils/constants"
+
+
+const styling= {
+  backgroundColor:"rgb(243 217 217)"
+}
+
+const RestaurantCard= (props) =>{                             {/* (or) const RestaurantCard = ({resName,cuisine}) */}
+const {resData} =  props
+const{name, cuisines,ncloudinaryImageId, costForTwo,avgRating, sla:{deliveryTime}}=resData?.info
+
+return(
+    <div className="res-card" style={styling}>
+      <img 
+      className="res-logo"
+      alt="res-logo" 
+      src={ CDN_URL + resData.info.cloudinaryImageId }/>
+      <h3>{name}</h3>                                     {/* (or) <h3> {res.Name} </h3> */}
+      <h5>{cuisines.join(", ")}</h5>                                 
+      <h5>{avgRating}</h5>
+      <h5>{costForTwo }</h5>
+      <h5>{deliveryTime} minutes </h5>
+    </div>
+  )
+}
+
+
+export default RestaurantCard;
