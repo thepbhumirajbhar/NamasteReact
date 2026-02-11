7161feda3dafa6ad779a6c2aa31e97ce74858c6e
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-11 17:51:43 +0530

    corsProxy.io to bypass cors error

--- Video 6/components/Body.js
+++ Video 6/components/Body.js
@@ -20,7 +20,7 @@ const Body= () =>{
   },[]);
 
   const fetchData = async() => {
-    const data= await fetch("https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
+    const data= await fetch("https://corsproxy.io/?url=https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING");
 
     const json=await data.json();
 
