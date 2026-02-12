5fe2314ca610eae690894afefa22a4bafdcf38f2
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-12 19:16:30 +0530

    useRouteError() hook

--- Video 7/components/Error.js
+++ Video 7/components/Error.js
@@ -1,8 +1,15 @@
+import { useRouteError } from "react-router-dom";
+//* hook provided by react router library: gives more info about the error, that info then further can be shown to the user.
+
+
 const Error=() =>{
+  const err= useRouteError();
+  console.log(err)
   return(
     <div>
       <h1>OOPS..!</h1>
       <h2>Something went wrong...</h2>
+      <h3> {err.status}: {err.statusText}</h3>
     </div>
   )
 }
