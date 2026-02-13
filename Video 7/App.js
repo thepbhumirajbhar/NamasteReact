4148e87308e038c22896cb8d500209c4873d0ef8
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-13 14:35:45 +0530

    adding restaurantmenu path

--- Video 7/App.js
+++ Video 7/App.js
@@ -9,13 +9,13 @@ import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 //* RouterProvider provides the routing configuration that we have create to our app.
 /* 
 ! Outlet is the component given to us, it fills itself according to the path.
-! GOAL= to keep the header intact in about, contact page.
 */
 
 
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 
 
@@ -45,6 +45,10 @@ const appRouter=createBrowserRouter([
     {
     path: "/contact",
     element: <Contact />,
+    },
+    {
+    path: "/restaurant/:resId",
+    element: <RestaurantMenu />,
     }
   ]
   },
