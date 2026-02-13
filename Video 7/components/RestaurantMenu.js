9887c1ae6cd1fdd9a15aa3d3d056f76ddbd8fe95
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-13 14:30:16 +0530

    making a restaurant menu component

---
+++ Video 7/components/RestaurantMenu.js
@@ -0,0 +1,15 @@
+const RestaurantMenu=() => {
+  return (
+    <div className="menu ">
+      <h1>Name of the Restaurant</h1>
+      <h3>Menu</h3>
+      <ul>
+        <l1>Biryaani</l1>
+        <l1>Burger</l1>
+        <l1>Butter Chicken</l1>
+      </ul>
+    </div>
+  )
+}
+
+export default RestaurantMenu; 
