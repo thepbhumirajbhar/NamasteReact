e60da4a7539cbb5b7b19a6cef311a4bdda12870a
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-14 17:16:24 +0530

    data from foodfire api fetched for a restaurant ID.

--- Video 7/components/RestaurantMenu.js
+++ Video 7/components/RestaurantMenu.js
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 const RestaurantMenu=() => {
+  
+  useEffect(()=>{
+    fetchMenu()
+  },[]);
+
+  const fetchMenu = async ()=>{
+    const data = await fetch(
+        "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=766502");
+
+    const json = await data.json();
+    console.log(json)
+  };
+
+
   return (
     <div className="menu ">
       <h1>Name of the Restaurant</h1>
