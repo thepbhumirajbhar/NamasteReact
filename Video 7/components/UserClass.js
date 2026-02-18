a2d4059e9d3eba26dcb315857ef18a889caad364
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 16:27:25 +0530

    update: state variable added

--- Video 7/components/UserClass.js
+++ Video 7/components/UserClass.js
@@ -5,6 +5,11 @@ class UserClass extends React.Component{
 
   constructor(props){
     super(props)
+
+    //way to create state variables in class based component(no hooks are called)
+    this.state={
+      count:0,
+    };
   }
 
 
@@ -17,6 +22,7 @@ class UserClass extends React.Component{
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contact info: @bhumirajbhar</h3>
+        <h2>Count:{this.state.count }</h2>
       </div>
       
 
