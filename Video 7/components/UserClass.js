c29854e130a1f34367de62338732dc5b2b7ba3a0
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 16:08:20 +0530

    update: destructuring the prop call

--- Video 7/components/UserClass.js
+++ Video 7/components/UserClass.js
@@ -9,12 +9,17 @@ class UserClass extends React.Component{
 
 
   render(){
+    const{name, location} =this.props
+
+
     return (
       <div className="user-card">
-      <h2>Name: {this.props.name}</h2>
-      <h3>Location: Delhi</h3>
-      <h3>Contact info: @bhumirajbhar</h3>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
+        <h3>Contact info: @bhumirajbhar</h3>
       </div>
+      
+
     )
   }
 }
