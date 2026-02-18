6a4166b09cd75ad9d4df7b379df125fa45afa1ee
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 15:53:09 +0530

    update: papssing prop to functional component

--- Video 7/components/About.js
+++ Video 7/components/About.js
@@ -1,11 +1,15 @@
 import User from "./User";
+import UserClass from "./UserClass";
+
 
 const About= () =>{
   return(
     <div>
       <h1>ABOUT US</h1>
       <h2>Get to know about our company</h2>
-      <User />
+      <User name={"Bhumi (Functional)"}/>
+
+      <UserClass name={"Bhumi (Class)"}/>
     </div>
   )
 }
