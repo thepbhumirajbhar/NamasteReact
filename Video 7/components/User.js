be99f5ff1780790fe701cc97d7b45abf09c6055f
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 15:16:51 +0530

    initial commit

---
+++ Video 7/components/User.js
@@ -0,0 +1,11 @@
+const User= () =>{
+  return (
+    <div className="user-card">
+      <h2>Name: BHUMI </h2>
+      <h3>Location: Delhi</h3>
+      <h3>Contact info: @bhumirajbhar</h3>
+    </div>
+  )
+}
+
+export default User;
