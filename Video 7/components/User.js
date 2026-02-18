a549ed31fac0e226f4d653a7e52cde1535f6d687
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 16:28:08 +0530

    update: state variable using hooks (count btn)

--- Video 7/components/User.js
+++ Video 7/components/User.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
+
 const User= (props) =>{
+
+  const[count,setCount] = useState(0)
+
   return (
     <div className="user-card">
       <h2>Name: {props.name}</h2>
       <h3>Location: Delhi</h3>
       <h3>Contact info: @bhumirajbhar</h3>
+      <h2>Count: {count}</h2>
     </div>
   )
 }
