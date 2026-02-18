61d640e4c8e7f96c8907d819d64746df7997a917
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 16:48:04 +0530

    update: added the count increase btn

--- Video 7/components/User.js
+++ Video 7/components/User.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 const User= (props) =>{
 
   const[count,setCount] = useState(0)
+  // const[count2,setCount2] = useState(1)
 
   return (
     <div className="user-card">
@@ -11,6 +12,13 @@ const User= (props) =>{
       <h3>Location: Delhi</h3>
       <h3>Contact info: @bhumirajbhar</h3>
       <h2>Count: {count}</h2>
+      <button 
+        onClick={()=>{
+          setCount(count+1)
+        }}>
+        Count Updater
+      </button>
+      {/* <h2>Count- 2: {count2}</h2> */}
     </div>
   )
 }
--- Video 7/components/UserClass.js
+++ Video 7/components/UserClass.js
@@ -9,6 +9,7 @@ class UserClass extends React.Component{
     //way to create state variables in class based component(no hooks are called)
     this.state={
       count:0,
+      // count2: 1,
     };
   }
 
@@ -23,6 +24,19 @@ class UserClass extends React.Component{
         <h3>Location: {location}</h3>
         <h3>Contact info: @bhumirajbhar</h3>
         <h2>Count:{this.state.count }</h2>
+
+
+        <button onClick={()=>{
+           //NEVER UPDATE STATE VARIABLES DIRECTLY
+           this.setState({
+            count: this.state.count + 1,
+           })
+        }}>
+          Count Increase
+        </button>
+
+
+        {/* <h2>Count-2:{this.state.count2 }</h2> */}
       </div>
       
 
