3e21cfd6601002f87d215976720c3cd215c0ce13
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 15:37:59 +0530

    added class based component

---
+++ Video 7/components/UserClass.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+class UserClass extends React.Component{
+  render(){
+    return (
+      <div className="user-card">
+      <h2>Name: BHUMI </h2>
+      <h3>Location: Delhi</h3>
+      <h3>Contact info: @bhumirajbhar</h3>
+      </div>
+    )
+  }
+}
+
+export default UserClass;
