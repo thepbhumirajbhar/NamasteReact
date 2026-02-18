39ab4b97639134a5cedc2b75a268504f88bdb5c5
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-18 15:53:41 +0530

    update: receiving props in class based component

--- Video 7/components/UserClass.js
+++ Video 7/components/UserClass.js
@@ -2,10 +2,16 @@ import React from "react"
 
 
 class UserClass extends React.Component{
+
+  constructor(props){
+    super(props)
+  }
+
+
   render(){
     return (
       <div className="user-card">
-      <h2>Name: BHUMI </h2>
+      <h2>Name: {this.props.name}</h2>
       <h3>Location: Delhi</h3>
       <h3>Contact info: @bhumirajbhar</h3>
       </div>
