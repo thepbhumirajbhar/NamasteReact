ed42e562c80368227328a9eb02677728e95acbed
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-20 01:32:00 +0530

    update: removed fetch api functionality

--- Video 7/components/RestaurantMenu.js
+++ Video 7/components/RestaurantMenu.js
@@ -2,29 +2,16 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 //! GOAL: to load the fetched data to the UI => State Variable used.
 const RestaurantMenu=() => {
-  const[resInfo, setResInfo] = useState(null)
 
   const {resId} = useParams();
   
-
-  useEffect(()=>{
-    fetchMenu()
-  },[]);
-
-  const fetchMenu = async ()=>{
-    const data = await fetch(MENU_API+ resId);
-    
-
-    const json = await data.json();
-
-    console.log(json)
-    setResInfo(json.data)
-    console.log("API URL =>", MENU_API + resId);
-  };
+  const resInfo = useRestaurantMenu(resId)
+  
 
   if (resInfo===null) {
     return <Shimmer />};
