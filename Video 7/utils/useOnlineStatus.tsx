2121ac971dfb029210d6b5496a70c60c4ae0167a
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-21 16:50:44 +0530

    update: added online/offline status checker

--- Video 7/utils/useOnlineStatus.tsx
+++ Video 7/utils/useOnlineStatus.tsx
@@ -1,7 +1,30 @@
 
+import { useEffect, useState } from "react";
+
 
 const useOnlineStatus = () =>{
-  return{
-    
-  }
+
+  const [onlineStatus, setOnlineStatus] = useState(true)
+
+
+    //check if online
+   useEffect(() => {
+      window.addEventListener("offline",() =>{
+          setOnlineStatus(false);
+      });
+
+       window.addEventListener("online",() =>{
+          setOnlineStatus(true);
+      });
+   },[])
+
+
+
+    // boolean value
+  return onlineStatus;
 }
+
+
+export default useOnlineStatus;
+
+
