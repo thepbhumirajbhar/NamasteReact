8e576983f47faa8b73f9e5e450396598d2e164f5
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-22 19:26:40 +0530

    renamed

--- Video 7/components/Error.tsx
+++ Video 7/components/Error.tsx
@@ -3,11 +3,11 @@ import { useRouteError } from "react-router-dom";
 
 
 const Error=() =>{
-  const err= useRouteError();
+  const err= useRouteError() as any;
   console.log(err)
   return(
     <div>
-      <h1>OOPS..!</h1>
+      <h1>OOPS..!</h1> 
       <h2>Something went wrong...</h2>
       <h3> {err.status}: {err.statusText}</h3>
     </div>
