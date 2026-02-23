480a6844ba6326faf77ecc03fd8d97faba28c26c
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-23 13:48:13 +0530

    update: added grocery path

--- Video 7/App.tsx
+++ Video 7/App.tsx
@@ -16,6 +16,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Grocery from "./components/Grocery"
 
 
 
@@ -45,6 +46,10 @@ const appRouter=createBrowserRouter([
     {
     path: "/contact",
     element: <Contact />,
+    },
+    {
+    path: "/grocery",
+    element: <Grocery />,
     },
     {
     path: "/restaurant/:resId",              
