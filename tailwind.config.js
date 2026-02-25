dd14a6d9b2d91c2508955f095818e78dbb1752fb
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-25 15:01:17 +0530

    update: tailwind initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Video 7/**/*.{html,js,jsx,ts,tsx}", 
+    // Or, to make it scan EVERY folder in your project, use this instead:
+    // "./**/*.{html,js,jsx,ts,tsx}" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
