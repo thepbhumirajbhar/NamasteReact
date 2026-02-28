7261de1451490af2cf4578dde1b7450e10f1938e
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-28 19:18:46 +0530

    update: setting accordian header

--- Video 7/components/RestaurantCategory.tsx
+++ Video 7/components/RestaurantCategory.tsx
@@ -1,3 +1,5 @@
+import ItemList from "./ItemList";
+
 type CategoryData={
   data: any;
 }
@@ -5,25 +7,23 @@ type CategoryData={
 
 
 const RestaurantCategory= ({data}: CategoryData) =>{
-  console.log("list of each category one by one", data)
+  //console.log("list of each category one by one", data)
+  
   return(
     <div>
       {/* ACCORDIAN HEADING */}
-      <div className="w-6/12 mx-auto my-4 p-3 bg-gray-300 shadow-md shadow-gray-600 flex justify-between">
-        <span className="font-bold">
-          {data.title} ({data.itemCards.length})
-        </span>
-        
-        <span> ↓</span>
-      </div>
-    
-
-    {/* ACCORDIAN BODY */}
-    <div>
-
+      <div className="w-6/12 mx-auto my-4 p-3 bg-gray-300 shadow-md shadow-gray-600">
+        <div className="flex justify-between">
+          <span className="font-bold text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span> ↓ </span>
+        </div>
+      
+      {/* ACCORDIAN BODY */}
+      <ItemList items = {data.itemCards} />
     </div>
-    
-    </div>
+  </div>  
   )
 }
 
