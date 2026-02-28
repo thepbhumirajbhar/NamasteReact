f369e69247cba133d35f38e28a303a1bbd97d2b3
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-28 12:34:34 +0530

    update: Higher order component use case(res. card with pro,oted label)

--- Video 7/components/RestaurantCard.tsx
+++ Video 7/components/RestaurantCard.tsx
@@ -23,14 +23,23 @@ type RestaurantCardProps={
 
 
 
-const RestaurantCard= (props: RestaurantCardProps) =>{                {/* (or) const RestaurantCard = ({resName,cuisine}) */}
+
+
+const RestaurantCard = (props: RestaurantCardProps) =>{               
+   {/* (or) const RestaurantCard = ({resName,cuisine}) */}
 const {resData} =  props
-const{name, cuisines,cloudinaryImageId, costForTwo,avgRating, sla:{deliveryTime}}=resData?.info
+
+const{name, 
+      cuisines,
+      cloudinaryImageId, 
+      costForTwo,
+      avgRating, 
+      sla:{deliveryTime}}=resData?.info
 
 return(
     <div className="m-3 p-4 w-[250px] h-full bg-green-100 shadow-md shadow-gray-700 rounded-md border-2 border-green-900 hover:bg-gray-300">
       <img 
-      className="rounded-md"
+      className="rounded-md h-56 w-full"
       alt="res-logo" 
       src={ CDN_URL + resData.info.cloudinaryImageId }/>
       <h3 className="font-bold underline text-lg text-green-950 py-3">{name}</h3>                                     {/* (or) <h3> {res.Name} </h3> */}
@@ -42,5 +51,27 @@ return(
   )
 }
 
+
+
+/* 
+! Higher Order Component
+! i/p = restaurantCard; o/p = restaurantcard with label promoted
+.
+. 
+. 
+*/
+
+export const withPromotedLabel = (RestaurantCard: React.ComponentType<RestaurantCardProps>) =>{
+  return (props: RestaurantCardProps) => {
+    return(
+      <div>
+        <label> Promoted label </label>
+        <RestaurantCard {...props}/>
+      </div>
+
+    )
+  }
+
+}
 
 export default RestaurantCard;
