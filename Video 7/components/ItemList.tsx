fb6f99010e5b3b9bd111bece3a2c1f9540baebbc
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-02-28 20:22:24 +0530

    update: setting up accordian body

--- Video 7/components/ItemList.tsx
+++ Video 7/components/ItemList.tsx
@@ -1,11 +1,32 @@
+import { CDN_URL } from "../utils/constants";
+
 type ItemType={
-  items: any;
+  items: any[];
 }
 
-const ItemList = (items:ItemType) =>{
+const ItemList = ({items}:ItemType) =>{
+  console.log("each item from ItemList is: ", items)
+
   return(
     <div>
-      category items
+      {items.map((item:any)=>(
+        <div 
+        key={item.card.info.id}
+        className="m-2 p-2 border-b-2 border-gray-600 text-left">
+
+          <img 
+          src={CDN_URL + item.card.info.imageId}
+          className="w-16"/>
+
+          <div className="pb-2">
+            <span className="font-semibold">{item.card.info.name}</span>
+            <span> - ₹{ item.card.info.price/100 || (item.card.info.defaultPrice/100)}</span>
+          </div>
+
+          <p className="text-sm text-slate-700">{item.card.info.description}</p>
+        </div>
+        
+      ))}
     </div>
   )
 }
