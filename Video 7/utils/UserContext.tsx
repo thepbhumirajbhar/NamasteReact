155d4575251ee02762825d56c07c5ab29c5b37ba
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-01 21:27:41 +0530

    added setUserName

--- Video 7/utils/UserContext.tsx
+++ Video 7/utils/UserContext.tsx
@@ -3,7 +3,8 @@ import { createContext } from "react";
 
 //this UserContext can be accessed from anywhere in the app; bypassing the problem of prop drilling.
 const UserContext= createContext({
-  loggedInUser: "Guest"
+  loggedInUser: "Guest",
+  setUserName: (name: string)=>{},
 });
 
 export default UserContext;
