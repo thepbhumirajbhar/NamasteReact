3b1bf4e629fb24930c4adffd51f048647709d70b
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-02 14:06:51 +0530

    cartSlice created

---
+++ Video 7/utils/cartSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+//* createSlice is a function that takes a configuration to create a slice
+const cartSlice = createSlice({
+  name: 'cart',
+
+  initialState: {
+    items: []
+  },
+
+  reducers: {
+    //* write reducer fun corresponding to 'each action'
+
+    // takes 2 parameters: modifies the state based on action
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+
+    removeItem: (state, action) =>{
+      state.items.pop()
+    },
+
+    clearCart:(action, state) => {
+      state.items.length = 0;
+    }
+
+  }
+
+})
+
+
+// exporting actions and reducers.
+export const {addItem, removeItem, clearCart} = cartSlice.actions;
+export default cartSlice.reducer;
