4ec7eaeb41bf0eb03ab37a79d4e698ec3424d21f
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-02 19:44:57 +0530

    update: onclick cart functionality using redux store

--- Video 7/components/ItemList.tsx
+++ Video 7/components/ItemList.tsx
@@ -1,51 +1,69 @@
+import { useDispatch } from "react-redux";
 import { CDN_URL } from "../utils/constants";
+import { addItem } from "../utils/cartSlice";
 
 type ItemType={
   items: any[];
 }
 
+
+
+
 const ItemList = ({items}:ItemType) =>{
-  console.log("each item from ItemList is: ", items)
+    //console.log("each item from ItemList is: ", items)
 
-  return(
-    <div>
-      {items.map((item:any)=>(
-        <div
-        key={item.card.info.id}
-        className="m-2 border-b-2 border-gray-600 text-left flex justify-between items-center pb-2">
-          <div className="w-9/12">
-            <div className="py-2">
-               <span className="font-semibold">{item.card.info.name}</span>
-               <span> - ₹{ item.card.info.price/100 || (item.card.info.defaultPrice/100)}</span>
-            </div>
 
-            <p className="p-2 text-sm text-slate-700">{item.card.info.description}</p>
+    const dispatch= useDispatch();
 
-        </div>
-          
+    const handleAddItem = (item: any) =>{
+    //onclick -> Dispatch an action(first import dispatch via useDispatch)
+    //! whatever is put inside addItem goes to cartSlice-> addItem-> action.payload
+    dispatch(addItem(item))
+    }  
 
-       
-        <div className="w-3/12">
-        {/* Ek chota sa check: Agar API mein imageId hai tabhi dikhao, warna khali chhod do */}
-            {
-              item.card.info.imageId && (
-                <img 
-                  src={CDN_URL + item.card.info.imageId}
-                  alt={item.card.info.name}
-                  className="w-full shadow-md shadow-black h-24 rounded-md"/>)}
-                  <div>
-                    <button className="m-auto p-1 font-bold text-sm bg-green-950 text-neutral-50 flex rounded-md">
-                       Add +
-                    </button>
-                  </div>
-                
-        </div>
-     </div>
+
+
+    return(
+      <div>
+        {items.map((item:any)=>(
+          <div
+          key={item.card.info.id}
+          className="m-2 border-b-2 border-gray-600 text-left flex justify-between items-center pb-2">
+            <div className="w-9/12">
+              <div className="py-2">
+                 <span className="font-semibold">{item.card.info.name}</span>
+                 <span> - ₹{ item.card.info.price/100 || (item.card.info.defaultPrice/100)}</span>
+              </div>
         
-      ))}
-    </div>
-
-  )
-}
-
+              <p className="p-2 text-sm text-slate-700">{item.card.info.description}</p>
+        
+          </div>
+            
+        
+        
+          <div className="w-3/12">
+          {/* Ek chota sa check: Agar API mein imageId hai tabhi dikhao, warna khali chhod do */}
+              {
+                item.card.info.imageId && (
+                  <img 
+                    src={CDN_URL + item.card.info.imageId}
+                    alt={item.card.info.name}
+                    className="w-full shadow-md shadow-black h-24 rounded-md"/>)}
+  
+                    <div>
+                      <button className="m-auto p-1 font-bold text-sm bg-green-950 text-neutral-50 flex rounded-md"
+                              onClick={()=>{handleAddItem(item)}}>
+                         Add +
+                      </button>
+                    </div>
+                  
+          </div>
+       </div>
+          
+        ))}
+      </div>
+  
+    )
+} 
+  
 export default ItemList;
