6b283c45a07ae3f835719d1f126b3301bd6fe1ad
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-02 15:23:57 +0530

    update: add cart slice to the store

--- Video 7/utils/appstore.tsx
+++ Video 7/utils/appstore.tsx
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./cartSlice";
 
 
-const appStore = configureStore(
-  //add slices here;
-  
+const appStore = configureStore({
+  //* this reducer consists of slice-reducers; modifies the store
+  reducer:{
+    cart: cartReducer,
+  },
+}
 );
 
 export default appStore;
