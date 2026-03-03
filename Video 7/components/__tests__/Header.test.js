22aa1af068b5b5bba1fcfcf9ddc8b67432a8dea6
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-04 03:22:02 +0530

    importing BrowserRouter so it renders 'Link to' tag

--- Video 7/components/__tests__/Header.test.js
+++ Video 7/components/__tests__/Header.test.js
@@ -1,14 +1,16 @@
 import { render } from "@testing-library/react"
 import Header from "../Header"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 
 it("should load Header component with a Login button", () =>{
 
   render(
-    <Provider store={appStore}>
-      (<Header/ >)
-    </Provider>
+    <BrowserRouter>
+      <Provider store={appStore}>
+        (<Header/ >)
+      </Provider>
+    </BrowserRouter>
   )
-
 
 })
