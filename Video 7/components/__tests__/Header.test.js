477caccff0a371f012ddc45ca8fb4255e1260a2e
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-04 14:10:26 +0530

    login to logout change testing(FireEvent)

--- Video 7/components/__tests__/Header.test.js
+++ Video 7/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Header from "../Header"
 import { Provider } from "react-redux"
 import appStore from "../../utils/appstore"
@@ -44,5 +44,26 @@ it("should load Header component with Cart item 0 button", () =>{
   const cartItems = screen.getByText(/Cart/);
 
   expect(cartItems).toBeInTheDocument();
+
+})
+
+
+it("should should change Login to Logout on click", () =>{
+
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        (<Header/ >)
+      </Provider>
+    </BrowserRouter>
+  )
+
+  const logInBtn = screen.getByRole("button", {name: 'LOG IN'})
+
+  fireEvent.click(logInBtn)
+
+  const logOutBtn = screen.getByRole("button", {name: 'LOG OUT'})
+
+  expect(logOutBtn).toBeInTheDocument();
 
 })
